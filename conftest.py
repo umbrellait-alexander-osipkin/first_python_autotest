1e955544d31f6d564c4bdda8f53ff53e683ad5e8
alexanderosipkin <alexander.osipkin@umbrellait.com>
2024-05-28 10:22:07 +0300

    adding pytest fixtures

---
+++ conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from selene import browser
+from selenium import webdriver
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_cfg():
+    browser.config.base_url = 'https://www.google.com/'
+    driver_options = webdriver.ChromeOptions()
+    browser.config.driver_options = driver_options
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
+    browser.config.timeout = 15
