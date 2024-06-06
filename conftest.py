8a640204c0a35dbaf13d898e33cf07f179f03940
alexanderosipkin <alexander.osipkin@umbrellait.com>
2024-06-06 16:02:20 +0300

    adding pytest fixtures

--- conftest.py
+++ conftest.py
@@ -6,9 +6,13 @@ from selenium import webdriver
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_cfg():
-    browser.config.base_url = 'https://www.google.com/'
+    browser.config.base_url = 'https://www.google.com'
     driver_options = webdriver.ChromeOptions()
     browser.config.driver_options = driver_options
     browser.config.window_height = 1080
     browser.config.window_width = 1920
     browser.config.timeout = 15
+
+    yield
+
+    browser.quit()
--- test_first.py
+++ test_first.py
@@ -2,6 +2,6 @@ from selene import browser, have, be
 
 
 def test_google_search():
-    browser.open('https://www.google.com/')
+    browser.open('/')
     browser.element('[name="q"]').should(be.blank).type('UmbrellaIT').press_enter()
     browser.element('[id="search"]').should(have.text('Umbrella IT | Разработка Веб и Мобильных приложений'))
